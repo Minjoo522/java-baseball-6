1529c757ddbebbff0d72059e1f31f5c9e2237ebe
Minjoo <m0522inju@gmail.com>
2023-12-11 17:56:45 +0900

    fix: 숫자 입력 요청 부분 개행되지 않도록 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,6 +3,6 @@ package baseball.view;
 public class OutputView {
     public static void printStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.println("숫자를 입력해 주세요 : ");
+        System.out.print("숫자를 입력해 주세요 : ");
     }
 }
